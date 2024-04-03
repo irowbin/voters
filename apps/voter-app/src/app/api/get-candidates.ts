05318eaac33003040522c9942ae568b8724a1545
robin <irowbin@gmail.com>
2024-04-03 18:46:53 +0545

    doc: update readme.md

--- apps/voter-app/src/app/api/get-candidates.ts
+++ apps/voter-app/src/app/api/get-candidates.ts
@@ -8,10 +8,4 @@ export const getCandidates = (): Person[] => [
   { id: uuidV4(), name: 'Talia Oakley', votes: 0, voted: false },
   { id: uuidV4(), name: 'Ivan Adler', votes: 0, voted: false },
   { id: uuidV4(), name: 'Orion Granger', votes: 0, voted: false },
-  { id: uuidV4(), name: 'Emmett Jennings', votes: 0, voted: false },
-  { id: uuidV4(), name: 'Phoebe Pierce', votes: 0, voted: false },
-  { id: uuidV4(), name: 'Orion Dunn', votes: 0, voted: false },
-  { id: uuidV4(), name: 'Orion Thorne', votes: 0, voted: false },
-  { id: uuidV4(), name: 'Rosalie Jennings', votes: 0, voted: false },
-  { id: uuidV4(), name: 'Aiden Ellsworth', votes: 0, voted: false },
 ]
--- apps/voter-app/src/app/api/get-voters.ts
+++ apps/voter-app/src/app/api/get-voters.ts
@@ -5,12 +5,6 @@ import { uuidV4 } from '../utils/uuid.utils'
 
 export const getVoters = (): Person[] => [
   { id: uuidV4(), name: 'Quentin Carmichael', votes: 0, voted: false },
-  { id: uuidV4(), name: 'Luna Redford', votes: 0, voted: false },
-  { id: uuidV4(), name: 'Orion Redford', votes: 0, voted: false },
-  { id: uuidV4(), name: 'Gideon Adler', votes: 0, voted: false },
-  { id: uuidV4(), name: 'Quentin Sullivan', votes: 0, voted: false },
-  { id: uuidV4(), name: 'Gideon Morrow', votes: 0, voted: false },
-  { id: uuidV4(), name: 'Helena Knight', votes: 0, voted: false },
   { id: uuidV4(), name: 'Miles Irwin', votes: 0, voted: false },
   { id: uuidV4(), name: 'Miles North', votes: 0, voted: false },
   { id: uuidV4(), name: 'Brielle Blackwood', votes: 0, voted: false },
